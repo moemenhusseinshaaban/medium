90a47b02a09261435841e8dd2ac1a719c3dbcd6d
reinaldo.rossetti <reinaldo.rossetti@inmetrics.com.br>
2020-01-11 12:13:57 -0300

    solução do atributo value preenchido no capybara

---
+++ capybara/solution_to_native_clear/01_test_with_send_keys.rb
@@ -0,0 +1,33 @@
+require 'capybara'
+require 'selenium-webdriver'
+require 'rspec'
+require 'benchmark'
+
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
+Capybara.default_max_wait_time = 15
+$browser = Capybara::Session.new(:firefox)
+$browser.visit("https://www.w3schools.com/code/tryit.asp?filename=GASWGTGYEHKK")
+
+def problema_do_clean
+
+  $browser.first('button', :text => 'Run »', :visible => true).click
+  $browser.within_frame('iframeResult', :visible=>true){
+    count = 0
+    begin
+      element01 = $browser.find('input[name="fname"]', :visible => true)
+      element02 = $browser.find('input[name="lname"]', :visible => true)
+      element01.native.clear
+      element01.send_keys "test 1"
+      element02.native.clear
+      element02.send_keys "test 2"
+    rescue => ex
+        print ex
+    end
+    }
+end
+
+problema_do_clean
+sleep 15 # para ver na tela a mudanca.
---
+++ capybara/solution_to_native_clear/03_test_with_set.rb
@@ -0,0 +1,30 @@
+require 'capybara'
+require 'selenium-webdriver'
+require 'rspec'
+require 'benchmark'
+
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
+Capybara.default_max_wait_time = 15
+$browser = Capybara::Session.new(:firefox)
+$browser.visit("https://www.w3schools.com/code/tryit.asp?filename=GASWGTGYEHKK")
+
+def problema_do_clean
+
+  $browser.first('button', :text => 'Run »', :visible => true).click
+  $browser.within_frame('iframeResult', :visible=>true){
+    begin
+      element01 = $browser.find('input[name="fname"]', :visible => true)
+      element02 = $browser.find('input[name="lname"]', :visible => true)
+      element01.set "test 1"
+      element02.set "test 2"
+    rescue => ex
+        print ex
+    end
+    }
+end
+
+problema_do_clean
+sleep 15 # para ver na tela a mudanca.
