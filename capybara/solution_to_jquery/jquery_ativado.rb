792fb671f98b22bd9f3e72f0884580d1d47d65f0
reinaldo.rossetti <reinaldo.rossetti@inmetrics.com.br>
2020-02-22 15:46:38 -0300

    removendo a linha que não precisava

--- capybara/solution_to_jquery/jquery_ativado.rb
+++ capybara/solution_to_jquery/jquery_ativado.rb
@@ -5,13 +5,12 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome )
 end
 
-$browser = Capybara::Session.new(:chrome);Capybara.default_max_wait_time = 20
+$browser = Capybara::Session.new(:chrome); Capybara.default_max_wait_time = 20
 $browser.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover')
 $browser.driver.browser.switch_to.frame ("iframeResult")
 
 def trigger_event(elem, evento)
   dir_atual = File.expand_path File.dirname(__FILE__) # pega o caminho do arquivo
-  query = elem.instance_variable_get(:@query).locator
   js = File.read("#{dir_atual}/jquery.min.js") # ler o arquivo jquery
   $browser.execute_script(js)
   $browser.execute_script("$(arguments[0]).trigger(\"#{evento}\")", elem)
