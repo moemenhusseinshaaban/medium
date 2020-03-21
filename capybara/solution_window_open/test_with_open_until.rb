0ca6a75a6cb0ecdd9968d97d71910ddea47cf424
reinaldo.rossetti <reinaldo.rossetti@inmetrics.com.br>
2020-03-21 14:20:10 -0300

    implementando uma espera de janela.

---
+++ capybara/solution_window_open/test_with_open_until.rb
@@ -0,0 +1,58 @@
+require 'capybara'
+require 'capybara/dsl'
+
+# Configuracao que vai ficar no env.rb
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
+Capybara.default_driver = :firefox
+
+Capybara.configure do |config|
+  config.app_host = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_open"
+  config.default_max_wait_time = 30
+end
+#######################################
+
+# Nossa Page usamos o DSL instancia
+
+class TestWindows
+
+  include Capybara::DSL
+
+  # o visit vai ficar no step definition, uma adaptacao aqui
+  def load_page
+    page.visit('')
+  end
+
+  def problem_window
+    # esta dentro do primeiro frame, e retorna a instancia da segunda tela
+    page.within_frame('iframeResult', :visible => true) {
+      $primeira_janela = page.current_window
+      $segunda_janela = page.window_opened_by do
+        page.find('button[onclick*=myFunction]', :visible => true).click
+      end
+    }
+    # uso a segunda tela e dou um click
+    page.within_window $segunda_janela do
+      puts "Segunda tela usando: #{page.has_text? "LEARN HTML"}"
+      # abre uma terceira janela
+      page.find("a[href='/html/tryit.asp?filename=tryhtml_default']", visible: true).click
+      end
+    count = 0
+    until count > 4
+      puts page.windows.count
+      break if page.windows.count == 3
+      sleep 1; count+=1
+    end
+    puts "Fecha a terceira janela" # sem o contador iria gerar erro
+    page.switch_to_window page.windows.last
+    page.windows.last.close # fecha a terceira janela
+    page.switch_to_window $primeira_janela
+  end
+end
+
+test = TestWindows.new
+test.load_page
+test.problem_window
+sleep 15 # para ver na tela a mudanca.
